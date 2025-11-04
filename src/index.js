116754f49429f677ef41b74b3530fd6b6aa28791
Borys Ziukov <ziukoff1985@gmail.com>
2025-11-04 10:57:16 +0200

    Init commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
 'use strict';
-
-import { createFormElements } from './createForm.js';
-import { setupSubmitForm } from './submitForm.js';
-import { setupEmailValidation } from './validateEmail.js';
-
-createFormElements();
-
-setupEmailValidation('input[name="email"]');
-
-setupSubmitForm();
