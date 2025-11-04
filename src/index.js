e695b1be95dfcaf10c2c30cb9275905b97a6e4b6
Borys Ziukov <ziukoff1985@gmail.com>
2025-11-04 13:16:31 +0200

    Add general logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 'use strict';
+
+function getGCD(a, b) {
+    a = Math.abs(Math.round(a));
+    b = Math.abs(Math.round(b));
+
+    if (a === 0 && b === 0) {
+        console.log('GCD(0,0) is defined as 0 by convention');
+        return 0;
+    }
+    if (a === 0) return b;
+    if (b === 0) return a;
+
+    return getGCD(b, a % b);
+}
+
+console.log(getGCD(10, 15));
+console.log(getGCD(10, 0));
+console.log(getGCD(0, 0));
+console.log(getGCD(0, 10));
+console.log(getGCD(-10, -15));
+console.log(getGCD(10, -15));
+console.log(getGCD(0, -15));
+console.log(getGCD(-15, 0));
+console.log(getGCD(-14.6, 0));
+console.log(getGCD(0, -11.3));
+console.log(getGCD(192, 72));
