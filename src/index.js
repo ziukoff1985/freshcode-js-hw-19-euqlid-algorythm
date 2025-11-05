41fd242eee7d3785a804690cb98d9aa7de24f7c9
Borys Ziukov <ziukoff1985@gmail.com>
2025-11-05 13:05:55 +0200

    add functions for calculating GCD using a loop and recursion

--- src/index.js
+++ src/index.js
@@ -1,19 +1,60 @@
 'use strict';
 
-function getGCD(a, b) {
-    if (a === 0 && b === 0) {
-        console.log('GCD(0,0) is defined as 0 by convention');
-        return 0;
+const ERR_NOT_NUMBER = 'Arguments must be numbers';
+const ERR_NOT_FINITE = 'Arguments must be finite numbers';
+const ERR_NOT_INTEGER = 'Arguments must be integers';
+const ERR_NOT_POSITIVE = 'Arguments must be positive integers greater than 0';
+
+// Eqlid Algorithm with loop
+function getGCDwithLoop(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new TypeError(ERR_NOT_NUMBER);
     }
-
-    a = Math.abs(Math.round(a));
-    b = Math.abs(Math.round(b));
-
+    if (!Number.isFinite(a) || !Number.isFinite(b)) {
+        throw new RangeError(ERR_NOT_FINITE);
+    }
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {
+        throw new TypeError(ERR_NOT_INTEGER);
+    }
+    if (a <= 0 || b <= 0) {
+        throw new RangeError(ERR_NOT_POSITIVE);
+    }
     while (b !== 0) {
         [a, b] = [b, a % b];
     }
     return a;
 }
 
-console.log(getGCD(0, 0));
-console.log(getGCD(192, 72));
+try {
+    console.log(getGCDwithLoop(192, 72));
+    console.log(getGCDwithLoop(NaN, 72));
+} catch (error) {
+    console.log(error);
+}
+
+// Eqlid Algorithm with recursion
+function getGCDwithRecursion(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new TypeError(ERR_NOT_NUMBER);
+    }
+    if (!Number.isFinite(a) || !Number.isFinite(b)) {
+        throw new RangeError(ERR_NOT_FINITE);
+    }
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {
+        throw new TypeError(ERR_NOT_INTEGER);
+    }
+    if (a <= 0 || b <= 0) {
+        throw new RangeError(ERR_NOT_POSITIVE);
+    }
+    function gcd(x, y) {
+        return y === 0 ? x : gcd(y, x % y);
+    }
+    return gcd(a, b);
+}
+
+try {
+    console.log(getGCDwithRecursion(156, 104));
+    console.log(getGCDwithRecursion(12.5, 72));
+} catch (error) {
+    console.log(error);
+}
